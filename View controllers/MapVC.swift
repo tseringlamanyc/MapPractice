ad9ea95538fa576724fd74f1129c1f5a372c64d6
Tsering Lama <tseringlama@pursuit.org>
2020-01-03 23:14:25 -0500

    starting to implement MapKit

---
+++ View controllers/MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  MapVC.swift
+//  MapPractice
+//
+//  Created by Tsering Lama on 1/3/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+
+}
