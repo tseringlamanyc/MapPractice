53b26feb00b3ecbf41edd3453b357c0baa53413d
Tsering Lama <tseringlama@pursuit.org>
2020-01-03 22:53:14 -0500

    Initial Commit

---
+++ MapPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapPractice
+//
+//  Created by Tsering Lama on 1/3/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
