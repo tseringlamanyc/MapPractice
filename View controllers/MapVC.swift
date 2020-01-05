5d10d4a711314f5a57626832e22efd617c2195b0
Tsering Lama <tseringlama@pursuit.org>
2020-01-04 19:26:09 -0500

    map shows normally for some users

--- View controllers/MapVC.swift
+++ View controllers/MapVC.swift
@@ -9,28 +9,37 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     var persons: Result1!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userLocation = CLLocationCoordinate2D(latitude: Double(persons.location.coordinates.latitude)!, longitude: Double(persons.location.coordinates.longitude)!)
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: userLocation, span: span)
-            mapView.setRegion(region, animated: true)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = userLocation
-        annotation.title = persons.location.city
-        annotation.subtitle = persons.location.country
-        mapView.addAnnotation(annotation)
-     
+        
+        let userLat = Double(persons.location.coordinates.latitude)!
+        let userLon = Double(persons.location.coordinates.longitude)!
+        let initialLocation = CLLocation(latitude: userLat, longitude: userLon)
+        centerMapOnLocation(location: initialLocation)
+//        let userLocation = CLLocationCoordinate2D(latitude: userLat, longitude: userLon)
+//        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+//        let region = MKCoordinateRegion(center: userLocation, span: span)
+//        mapView.setRegion(region, animated: false)
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: userLat, longitude: userLon)
+//        annotation.title = persons.location.city
+//        annotation.subtitle = persons.location.country
+//        mapView.addAnnotation(annotation)
     }
     
     
+    let regionRadius: CLLocationDistance = 10
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+    mapView.setRegion(coordinateRegion, animated: true)
+    }
+
     
-    
+ 
 }
